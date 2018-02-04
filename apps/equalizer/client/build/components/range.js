dbe9e9811b440ec2cc230108e4a3fa6ade18ac89
Neha Abhishek <kr.abhishek@yahoo.com>
2018-02-05 00:15:27 +0530

    Adding build folder to git.

---
+++ apps/equalizer/client/build/components/range.js
@@ -0,0 +1,2 @@
+const $=React.createElement;export default(()=>$('div',{class:'range flex justsb flexc'},$('span',null,'-12db'),$('span',null,'0'),$('span',null,'+12db')));
+//# sourceMappingURL=range.js.map
---
+++ apps/equalizer/client/build/index.js
@@ -0,0 +1,2 @@
+import App from'./components/app.js';ReactDOM.render(React.createElement(App),document.body);
+//# sourceMappingURL=index.js.map
