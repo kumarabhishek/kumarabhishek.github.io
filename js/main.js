3ebdaf72ee472cf26e016696b75a84a16c65f790
Kumar Abhishek <kumarabhishek.opensource@github.com>
2018-07-10 20:45:26 +0530

    Refactoring SW.

--- js/main.js
+++ js/main.js
@@ -68,18 +68,7 @@ function sendMail(e) {
 
 function registerSW() {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function (reg) {
-            if (reg.installing) {
-                // console.log('Service worker installing');
-            } else if (reg.waiting) {
-                // console.log('Service worker installed');
-            } else if (reg.active) {
-                // console.log('Service worker active');
-            }
-        }).catch(function (error) {
-            // registration failed
-            // console.log('Registration failed with ' + error);
-        });
+        navigator.serviceWorker.register('/sw.js', { scope: '/' });
     }
 }
 
--- sw.js
+++ sw.js
@@ -1,36 +1,45 @@
-var __version__ = 'v1.1';
+var __version__ = 'v1.0.0';
+var __CACHED_URLS__ = [
+  '/',
+  '/css/style.css',
+  '/js/main.js',
+  '/css/icon1.png',
+  '/css/icon2.png',
+  '/css/icon5.png',
+  '/css/icon6.png',
+  '/css/icon7.png',
+  '/css/ka.jpg',
+  '/css/bg.jpg'
+];
 
-self.addEventListener('install', function(event) {
+  self.addEventListener('install', function(event) {
     self.skipWaiting();
     event.waitUntil(
       caches.open(__version__).then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/css/style.css',
-          '/js/main.js',
-          '/css/icon1.png',
-          '/css/icon2.png',
-          '/css/icon5.png',
-          '/css/icon6.png',
-          '/css/icon7.png',
-          '/css/ka.jpg',
-          '/css/bg.jpg'
-        ]);
+        return cache.addAll(__CACHED_URLS__);
       })
     );
   });
+
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if(key !== __version__){
+            caches.delete(key);
+          }
+        }));
+      })
+    );
+  });
+
   
   self.addEventListener('fetch', function(event) {
     event.respondWith(caches.match(event.request).then(function(response) {
-      // caches.match() always resolves
-      // but in case of success response will have value
       if (response !== undefined) {
         return response;
       } else {
         return fetch(event.request).then(function (response) {
-          // response may be used only once
-          // we need to save clone to put one copy in cache
-          // and serve second one
           let responseClone = response.clone();
           
           caches.open(__version__).then(function (cache) {
